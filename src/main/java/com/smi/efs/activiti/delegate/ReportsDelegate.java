69832d17bc8b32f5c59d971dbb6eccc269418b0f
Charles Lee <charles.lee@orangeandbronze.com>
2016-12-01 10:34:17 +0800

    added cron timing to start process

---
+++ src/main/java/com/smi/efs/activiti/delegate/ReportsDelegate.java
@@ -0,0 +1,17 @@
+package com.smi.efs.activiti.delegate;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class ReportsDelegate implements JavaDelegate {
+	
+	private static final Log LOGGER = LogFactory.getLog(ReportsDelegate.class);
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		LOGGER.info("Executing process [Reports]...");
+	}
+
+}
