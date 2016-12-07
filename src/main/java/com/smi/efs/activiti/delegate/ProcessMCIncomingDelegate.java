bc24ae56bef3fb9cac04d8e87e0f23bc30d663ec
Charles Lee <charles.lee@orangeandbronze.com>
2016-12-07 20:27:38 +0800

    added windows automation via image detection

--- src/main/java/com/smi/efs/activiti/delegate/ProcessMCIncomingDelegate.java
+++ src/main/java/com/smi/efs/activiti/delegate/ProcessMCIncomingDelegate.java
@@ -4,6 +4,10 @@ import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.sikuli.script.ImagePath;
+import org.sikuli.script.Screen;
+
+import com.smi.efs.activiti.ActivitiTestApplication;
 
 public class ProcessMCIncomingDelegate implements JavaDelegate {
 	
@@ -12,6 +16,11 @@ public class ProcessMCIncomingDelegate implements JavaDelegate {
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		LOGGER.info("Execution process [MC Incoming / Process T067 & T112 Files]...");
+		
+		ImagePath.add(ActivitiTestApplication.class.getCanonicalName() + "/images");
+		
+		Screen screen = new Screen();
+		screen.click("show_desktop.png");
 	}
 
 }
